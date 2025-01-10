9d295f387dc7f6cb7611a6e2112d9a2242ee1204
oscarlally <116345465+oscarlally@users.noreply.github.com>
2025-01-10 14:52:40 +0000

    Update roi_tck.py

--- roi_tck.py
+++ roi_tck.py
@@ -36,8 +36,6 @@ tck_list = [
     "mrtrix3-AF_EX.tck",
     "mrtrix3-MHA_R.tck",
     "mrtrix3-MHA_L.tck",
-    "mrtrix3-MHA_R.tck",
-    "mrtrix3-MHA_L.tck",
     "mrtrix3_R_Lip.tck",
     "mrtrix3_L_Lip.tck",
     "mrtrix3_L_OR_UNI.tck",
