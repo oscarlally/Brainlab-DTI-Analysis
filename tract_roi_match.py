0c32479e2692b5ea660f21247bfe1ce9589eaba3
oscarlally <116345465+oscarlally@users.noreply.github.com>
2026-01-02 12:03:47 +0000

    minor fix for spelling

--- tract_roi_match.py
+++ tract_roi_match.py
@@ -3,7 +3,7 @@ from tabulate import tabulate
 def roi_table():
     tracts = [
         ["Arcuate Fasciculus", "AF_[X]", "BROCA_[X]", "SLF_[X]", "AF_exclude", ""],
-        ["CST Motor Hand3", "[X]HandM1", "[X]-Ped", "[X]-Cap", "", ""],
+        ["CST Motor Hand", "[X]HandM1", "[X]-Ped", "[X]-Cap", "", ""],
         ["CST Sensory Hand/Foot", "[X]handS1", "[X]-Thal", "", "", "-stop so the tract stops at the thalamus"],
         ["Optic Radiation", "[X]-LGN", "[X]-latCalc", "[X]-latVentr", "OR_exclude", ""],
         ["CST Sensory/Motor Foot", "[X]FootM1", "[X]-Ped", "[X]-Cap", "", ""],
