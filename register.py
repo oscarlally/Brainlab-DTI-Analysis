9f9637f053136f7afa67e2b9c3482d1e514cd3f3
oscarlally <116345465+oscarlally@users.noreply.github.com>
2026-01-02 12:03:15 +0000

    Major update to registration process, including view commands for radiology reporting

--- register.py
+++ register.py
@@ -95,29 +95,40 @@ def zero_test(file_1, file_2):
         return 1
 
 
-def change_thresh(nii_dir, object_name, thresh, debug):
+def change_thresh(nii_dir, object_name, thresh, debug, base_file, registered_obj):
 
-    thresholded_obj = f"{nii_dir}{object_name}_registered_threshold_{thresh}.nii.gz"
-    chtrsh_cmd = f"fslmaths {nii_dir}{object_name}_registered.nii.gz -thr {thresh} {thresholded_obj}"
-    view_cmd = f"fsleyes {thresholded_obj}"
-    run(chtrsh_cmd)
-    if debug == 'debug':
-        run(view_cmd)
-    return thresholded_obj
+    if not registered_obj:
+        thresholded_obj = f"{nii_dir}{object_name}_registered_threshold_{thresh}.nii.gz"
+        chtrsh_cmd = f"fslmaths {nii_dir}{object_name}_registered.nii.gz -thr {thresh} {thresholded_obj}"
+    overlays = []
+    for i in os.listdir(nii_dir):
+        if 'reg_' in i:
+            overlays.append(i)
+    for i in overlays:
+        base_file += f' {nii_dir}{i}'
+    if not registered_obj:
+        view_cmd = f"fsleyes {base_file} {thresholded_obj}"
+        run(chtrsh_cmd)
+        if debug == 'debug':
+            run(view_cmd)
+            return thresholded_obj
+    else:
+        view_cmd = f"fsleyes {base_file} {registered_obj}"
+        if debug == 'debug':
+            run(view_cmd)
+        return registered_obj
 
 
-def actual_registration(debug, dependencies, pid):
+def pre_registration(debug, dependencies, pid, base_file):
 
     nii_dir = f"{current_dir}/mrtrix3_files/{pid}/nifti/"
-    misc_dir = f"{current_dir}/mrtrix3_files/{pid}/misc/"
-    b0_extract_nii = f"{nii_dir}extracted_b0.nii"
     nii_files = os.listdir(nii_dir)
     for i in nii_files:
         if 'bet' in i:
-            t1_bet_nii = f"{nii_dir}{i}"
+            bet_nii = f"{nii_dir}{i}"
 
     print()
-    paths_to_convert = [p for p in os.listdir(nii_dir) if 'NORM' in p]
+    paths_to_convert = [p for p in os.listdir(nii_dir) if 'NORM.' in p]
     print('Available tracts to register are are:')
     for idx, i in enumerate(paths_to_convert):
         print(f"{idx + 1}. {i}")
@@ -130,6 +141,35 @@ def actual_registration(debug, dependencies, pid):
     object_name = object_pre_name.split('.')[0]
 
     registered_object = f"{nii_dir}{object_name}_registered.nii.gz"
+    if registered_object.split('/')[-1] in os.listdir(nii_dir):
+        print()
+        mrview_cmd = input("Registration of this tract already found. Would you like to skip step and go straight to reporting? (y/n):  ")
+        if mrview_cmd == 'y':
+            print()
+            print("NB: If you are changing the threshold via fsleyes and you want to save that threshold, \n"
+                  f"then please save the thresholded tract in the mrtrix3_files/{pid}/nifti directory.")
+            thresholded = change_thresh(nii_dir, object_name, tract_to_reg, debug, base_file, registered_object)
+            print()
+            thresh_check = input('Would you like to save your new threshold from the terminal (y/n): ')
+            if thresh_check == 'y':
+                print()
+                thresh = input('Please type in the threshold value that you want to try: ')
+                thresholded = change_thresh(nii_dir, object_name, thresh, None, base_file, None)
+            return 0, thresholded
+        else:
+            correct_trans, registered = actual_registration(debug, pid, dependencies, object_nii, bet_nii, object_name,
+                                                            registered_object, base_file)
+            return correct_trans, registered
+    else:
+        correct_trans, registered = actual_registration(debug, pid, dependencies, object_nii, bet_nii, object_name,
+                                                        registered_object, base_file)
+        return correct_trans, registered
+
+
+def actual_registration(debug, pid, dependencies, object_nii, bet_nii, object_name, registered_object, base_file):
+    nii_dir = f"{current_dir}/mrtrix3_files/{pid}/nifti/"
+    b0_extract_nii = f"{nii_dir}extracted_b0.nii"
+    misc_dir = f"{current_dir}/mrtrix3_files/{pid}/misc/"
 
     fslhd_cmd_1 = f"fslhd {b0_extract_nii}"
     fslhd_cmd_2 = f"fslhd {object_nii}"
@@ -140,11 +180,11 @@ def actual_registration(debug, dependencies, pid):
     identical = zero_test(f"{misc_dir}extracted_b0.txt", f"{misc_dir}object.txt")
 
     # 1. register the dwi to T1 and get transform
-    trans_cmd_1 = f"flirt -in {b0_extract_nii} -ref {t1_bet_nii} -out {nii_dir}outvol.nii -omat {misc_dir}transform.mat -dof 6"
+    trans_cmd_1 = f"flirt -in {b0_extract_nii} -ref {bet_nii} -out {nii_dir}outvol.nii -omat {misc_dir}{object_name}_transform.mat -dof 6"
     run(trans_cmd_1)
 
     # 2. apply transform to object
-    trans_cmd_2 = f"flirt -in {object_nii} -ref {t1_bet_nii} -out {registered_object} -init {misc_dir}transform.mat -applyxfm"
+    trans_cmd_2 = f"flirt -in {object_nii} -ref {bet_nii} -out {registered_object} -init {misc_dir}{object_name}_transform.mat -applyxfm"
     run(trans_cmd_2)
 
     check = input('Do you want to threshold the registered tract? (y/n): ')
@@ -159,14 +199,13 @@ def actual_registration(debug, dependencies, pid):
             print("NB: If you are changing the threshold via fsleyes and you want to save that threshold, \n"
                   f"then please save the thresholded tract in the mrtrix3_files/{pid}/nifti directory.")
 
-            thresholded = change_thresh(nii_dir, object_name, thresh, debug)
+            thresholded = change_thresh(nii_dir, object_name, thresh, debug, base_file, None)
 
             print()
 
             happy = input('Do you want to re-threshold? (y/n): ')
 
             if happy == 'n':
-
                 break
 
     else:
@@ -176,22 +215,14 @@ def actual_registration(debug, dependencies, pid):
     """Potential for problems here given that the thresholded object is not being subtracted"""
 
     if identical == 0:
-
-        print()
-        cont = input('Continue? (y/n): ')
-
-        return 0, cont, thresholded
+        return 0, thresholded
 
     else:
-
-        print()
-        cont = input('Continue? (y/n): ')
-
-        return 1, cont, thresholded
+        return 1, thresholded
 
 
-def registration(debug, dependencies, pid):
+def registration(debug, dependencies, pid, base_file):
 
-    correct_trans, cont, registered = actual_registration(debug, dependencies, pid)
+    correct_trans, registered = pre_registration(debug, dependencies, pid, base_file)
 
-    return correct_trans, cont, registered
+    return correct_trans, registered
